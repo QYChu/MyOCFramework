d5058ef0826b84aa07ed865964e8dbfbfd746065
QYChu <18649756870@163.com>
2022-01-12 19:51:22 +0800

    1.0.1

---
+++ MyOCFramework.framework/Headers/MyOCFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyOCFramework.h
+//  MyOCFramework
+//
+//  Created by QYChu on 2022/1/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyOCFramework.
+FOUNDATION_EXPORT double MyOCFrameworkVersionNumber;
+
+//! Project version string for MyOCFramework.
+FOUNDATION_EXPORT const unsigned char MyOCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyOCFramework/PublicHeader.h>
+
+
---
+++ MyOCFramework.framework/Headers/QYCTestViewController.h
@@ -0,0 +1,17 @@
+//
+//  QYCTestViewController.h
+//  MyOCFramework
+//
+//  Created by QYChu on 2022/1/12.
+//
+
+#import <UIKit/UIKit.h>
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QYCTestViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
